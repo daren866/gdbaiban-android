bcb9b734969d36b0b2271032738f876af3a0027a
daren866 <xiao1xiao188@qq.com>
2026-07-04 17:29:18 +0800

    修复GitHub Release上传失败422错误

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
 }
 
 base {
-    archivesName.set("Gudaobaiban-v${android.defaultConfig.versionName}(${android.defaultConfig.versionCode})")
+    archivesName.set("Gudaobaiban-v${android.defaultConfig.versionName}-${android.defaultConfig.versionCode}")
 }
 
 dependencies {
